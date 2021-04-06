d206da6a7d2e3c7b80e3ddd6d75dc20b05dabf93
WX <42308734@qq.com>
2021-04-06 09:42:19 +0800

    fix:ComC Reducer

--- src/pages/ComC/index.js
+++ src/pages/ComC/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 const ComC = () => {
   const { ComCReducer: { name } } = useSelector(state => state)
+  const dispatch = useDispatch()
+  const handleClick = () => {
+    dispatch({ type: 'updateComC', payload: { name: 'ComC被点击了' } })
+  }
   return (
     <div>
       {name}
+      <button onClick={handleClick.bind(this)}>点击</button>
     </div>
   )
 }
